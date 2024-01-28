ca1fe380e3a4b43fb865a8490b98354629813be6
eunv <dmsql1091@naver.com>
2024-01-28 21:53:20 +0900

    feat : 일정 삭제 기능

--- src/components/calendar/show-calendar/ShowCalendar.jsx
+++ src/components/calendar/show-calendar/ShowCalendar.jsx
@@ -1,19 +1,30 @@
 import React from 'react'
 import styles from './ShowCalendar.module.scss'
+import { FaMinus } from "react-icons/fa";
+import { useRecoilState } from 'recoil';
+import { calendarInfoState } from './../../../states/calendar/calendarInfoState';
 
 const ShowCalendar = ({ calendar }) => {
+  const [calendarInfo, setCalendarInfo] = useRecoilState(calendarInfoState);
+
+  const delCalender = () => {
+    const updatedTravels = calendarInfo.travels.filter(item => item.locationName !== calendar.locationName);
+    setCalendarInfo({ ...calendarInfo, travels: updatedTravels });
+  };
+
   return (
-    <>
-      <div className={styles.container}>
-        <div>
-          <p>{calendar.locationName}</p>
-          <div className={styles.contents}>
-            <p>{calendar.memo}</p>
-          </div>
+    <div className={styles.container}>
+      <div className={styles.titles}>
+        <p>{calendar.locationName}</p>
+        <div className={styles.dele_btn} onClick={delCalender}>
+          <FaMinus />
         </div>
       </div>
-    </>
-  )
+      <div className={styles.contents}>
+        <p>{calendar.memo}</p>
+      </div>
+    </div>
+  );
 }
 
-export default ShowCalendar
+export default ShowCalendar;
