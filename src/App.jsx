ea34a0aecd0d364b6fabcc8c6c08fba90e39edbc
g6y116 <g6y116@naver.com>
2024-02-20 11:36:27 +0900

    feat : 서버 테스트

--- src/App.jsx
+++ src/App.jsx
@@ -24,7 +24,7 @@ function App() {
   return (
     <Routes>
       <Route path='/login' element={<Login />} />
-      <Route path='/login/oauth2/code/kakao' element={<KakaoRed />} />
+      <Route path='/login/kakao' element={<KakaoRed />} />
       {/* <Route path='/login/oauth2/code/naver' element={<NaverRed />} /> */}
 
       <Route path='/' element={<Layout />}>
