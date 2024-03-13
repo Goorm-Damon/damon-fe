8be21c54b4b1c49135726f35a1f2410e2a93c198
luz315 <315luz10@gmail.com>
2024-03-13 19:41:31 +0900

    refactor: 리뷰이미지 등록

--- src/pages/review/detail-review/DetailReview.jsx
+++ src/pages/review/detail-review/DetailReview.jsx
@@ -63,6 +63,16 @@ const DetailReview = () => {
     window.scrollTo(0, 1200);
   }
 
+  const [currentImageIndex, setCurrentImageIndex] = useState(0);
+
+  const showNextImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex + 1) % reviewInfo.imageUrls.length);
+  };
+  
+  const showPrevImage = () => {
+    setCurrentImageIndex((prevIndex) => (prevIndex - 1 + reviewInfo.imageUrls.length) % reviewInfo.imageUrls.length);
+  };
+  
   useEffect(() => {
     window.scrollTo(0, 0);
     // document.addEventListener('click', handleClickOutside);
@@ -93,9 +103,14 @@ const DetailReview = () => {
             {click && <DropDown setClick={setClick} reviewId={reviewId} />}
           </div>
           <section>
-            <div className={styles.image__box}>
-              {reviewInfo.imageUrls && <img className={styles.images} src={reviewInfo.imageUrls[0]} />}
-            </div>
+          <div className={styles.image__box}>
+  {reviewInfo.imageUrls && reviewInfo.imageUrls.length > 0 && (
+    <img src={reviewInfo.imageUrls[currentImageIndex]} alt="Review" className={styles.images} />
+  )}
+  <button onClick={showPrevImage} className={styles.prevButton}>이전</button>
+  <button onClick={showNextImage} className={styles.nextButton}>다음</button>
+</div>
+
             <div className={styles.icons}>
               {heart ? (
                 <FaHeart color='#F05D67' size={25} className={styles.icon} onClick={fetchLikeReview} />
